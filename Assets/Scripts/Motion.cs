bec02ff98f2e19684a4c7dd07e5dd4c8f780b22c
Atharva <atharvagondhali24@gmail.com>
2021-01-02 10:07:57 +0530

    game files added

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Coding.MultiplayerFPS
+{
+    public class Motion : MonoBehaviour
+    {
+        #region Variables
+
+        public float speed;
+        public float sprintModifier;
+        public float jumpForce;
+        private float baseFOV;
+        private float sprintFOVModifier = 1.25f;
+        private float movementCounter;
+        private float idleCounter;
+    
+        public Camera normalCam;
+        public Transform groundDetector;
+        public LayerMask ground;
+        public Transform weaponParent;
+        private Rigidbody rig;
+        private Vector3 weaponParentOrigin;
+        private Vector3 targetWeaponBobPosition;
+        
+
+        #endregion
+
+        #region Monobehaviour Callbacks
+
+        private void Start()
+        {
+            baseFOV = normalCam.fieldOfView;
+            Camera.main.enabled = false;
+            rig = GetComponent<Rigidbody>();
+            weaponParentOrigin = weaponParent.localPosition;
+        }
+
+        private void Update()
+        {
+            //Axles
+            float t_hmove = Input.GetAxisRaw("Horizontal");
+            float t_vmove = Input.GetAxisRaw("Vertical");
+
+
+            //Controls
+            bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jump = Input.GetKey(KeyCode.Space);
+
+            
+            //States
+            bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
+            bool isJumping = jump && isGrounded;
+            bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+
+
+            //Head Bob
+            if(t_hmove == 0 && t_vmove == 0) 
+            {
+                HeadBob(idleCounter, 0.025f, 0.025f);
+                idleCounter += Time.deltaTime;
+                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 2f); 
+            }
+            else if(! isSprinting)
+            {
+                HeadBob(movementCounter, 0.035f, 0.035f);
+                movementCounter += Time.deltaTime * 5f;
+                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 6f); 
+            }
+            else
+            {
+                HeadBob(movementCounter, 0.15f, 0.075f);
+                movementCounter += Time.deltaTime * 7f;
+                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 10f);
+            }
+
+        }
+
+        void FixedUpdate()
+        {
+            //Axles
+            float t_hmove = Input.GetAxisRaw("Horizontal");
+            float t_vmove = Input.GetAxisRaw("Vertical");
+            
+            
+            //Controls
+            bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jump = Input.GetKey(KeyCode.Space);
+
+            
+            //States
+            bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
+            bool isJumping = jump && isGrounded;
+            bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+            
+
+            //Jumping
+            if(isJumping)
+            {
+                rig.AddForce(Vector3.up * jumpForce);
+            }
+
+            
+            //Movement
+            Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
+            t_direction.Normalize();
+            
+            float t_adjustedSpeed = speed;
+            if( isSprinting ) t_adjustedSpeed *= sprintModifier;
+
+            Vector3 t_targetVelocity = transform.TransformDirection(t_direction) * t_adjustedSpeed * Time.deltaTime;
+            t_targetVelocity.y = rig.velocity.y;
+            rig.velocity = t_targetVelocity;
+        
+            
+            //Field of View
+            if(isSprinting) { normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier, Time.deltaTime * 8f); }
+            else {normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);}
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void HeadBob(float p_z, float p_x_intensity, float p_y_intensity)
+        {
+            targetWeaponBobPosition = weaponParentOrigin + new Vector3(Mathf.Cos(p_z) * p_x_intensity, Mathf.Sin(p_z * 2) * p_y_intensity, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace  Com.Coding.MultiplayerFPS
+{
+    public class Weapon : MonoBehaviour
+    {
+        #region Variables
+
+        public Gun[] loadout; 
+        public Transform weaponParent;
+        private int currentIndex;
+        private GameObject currentWeapon;
+
+        #endregion
+
+        #region Monobehaviour Callbacks
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+
+            if(currentWeapon != null)
+            {
+                Aim(Input.GetMouseButton(1));
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        void Equip(int p_ind)
+        {
+            if(currentWeapon != null) Destroy(currentWeapon);
+
+            currentIndex = p_ind;
+
+            GameObject t_newEquipment = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+            t_newEquipment.transform.localPosition = Vector3.zero;
+            t_newEquipment.transform.localEulerAngles = Vector3.zero;
+
+            currentWeapon = t_newEquipment;
+        }
+
+        void Aim(bool p_isAiming)
+        {
+            Transform t_anchor = currentWeapon.transform.Find("Anchor");
+            Transform t_state_ads = currentWeapon.transform.Find("States/ADS");
+            Transform t_state_hip = currentWeapon.transform.Find("States/Hip");
+
+            if(p_isAiming)
+            {
+                //Aim
+                t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+            }
+            else
+            {
+                //Hip
+                t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+
+            }
+        }
+
+        #endregion
+    }
+}
