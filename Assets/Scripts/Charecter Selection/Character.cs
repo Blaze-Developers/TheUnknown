6ebb7c3126c92f6cdfeb5685727c4364df6d4abb
AravinthSS07 <70379721+AravinthSS07@users.noreply.github.com>
2021-01-04 13:53:29 +0530

    Character Selection done

---
+++ Assets/Scripts/Charecter Selection/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character : MonoBehaviour
+{
+    public GameObject[] characters;
+    public int selectedcharacter = 0;
+
+    public void NextCharacter()
+    {
+        characters[selectedcharacter].SetActive(false);
+        selectedcharacter = (selectedcharacter + 1) % characters.Length;
+        characters[selectedcharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        characters[selectedcharacter].SetActive(false);
+        selectedcharacter--;
+        if (selectedcharacter < 0)
+        {
+            selectedcharacter += characters.Length;
+        }
+        characters[selectedcharacter].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("selectedCharacter", selectedcharacter);
+        SceneManager.LoadScene("SCENE");
+    }
+}
