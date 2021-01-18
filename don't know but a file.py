7c1bf7f2511eca0dd5275f0c082128ebb68882a3
AravinthSS07 <70379721+AravinthSS07@users.noreply.github.com>
2021-01-18 23:22:22 +0530

    Directory make o make

---
+++ don't know but a file.py
@@ -0,0 +1,10 @@
+#Allo Guys
+
+import random
+number = random.randint(1,9)
+live = 3
+
+while live > 0:
+  numberint = int(input("Enter a number between 1 to 9")
+  if numberint == number:
+    print("YOu did it")
