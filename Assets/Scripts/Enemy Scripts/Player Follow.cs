b726390f7d5cc057bec8ed2164a6cb497fdafb16
AravinthSS07 <70379721+AravinthSS07@users.noreply.github.com>
2021-02-03 16:04:36 +0530

    Enemy Scripts

---
+++ Assets/Scripts/Enemy Scripts/Player Follow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+	public Transform Character;
+	public float speed = 0.1F;
+	private Vector3 directionOfCharacter;
+	private bool challenged = false; // Check if touched
+
+	void Update()
+	{
+		if (challenged)
+		{
+			// Get direction to move towards
+			directionOfCharacter = Character.transform.position - transform.position;
+			directionOfCharacter = directionOfCharacter.normalized;
+
+			transform.Translate(directionOfCharacter * speed, Space.World);
+		}
+	}
+
+	// trigged on player hit
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			challenged = true;
+		}
+	}
+}
