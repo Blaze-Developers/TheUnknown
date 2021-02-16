b8f13ea87520b5bea857cda4a8dda115dd144c9b
AravinthSS07 <70379721+AravinthSS07@users.noreply.github.com>
2021-02-16 13:11:56 +0530

    Enemy AI

---
+++ Assets/Scripts/Health and Damahe Taker Script/SC_DamageReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_DamageReceiver : MonoBehaviour
+{
+    //This script will keep track of player HP
+    public float playerHP = 100;
+    public SC_CharacterController playerController;
+    public SC_WeaponManager weaponManager;
+
+    public void ApplyDamage(float points)
+    {
+        playerHP -= points;
+
+        if (playerHP <= 0)
+        {
+            //Player is dead
+            playerController.canMove = false;
+            playerHP = 0;
+        }
+    }
+}
